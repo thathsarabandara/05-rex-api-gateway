69f91a24bd3dff26ffd783aedbce24caee5739cd
Thathsara Bandara <thathsaraarumapperuma@gmail.com>
2026-07-15 22:57:25 +0530

    feat: add the middlewares for the repo: request id middleware

---
+++ src/middleware/request-id.middleware.ts
@@ -0,0 +1,14 @@
+import { Request, Response, NextFunction } from 'express';
+import { v4 as uuidv4 } from 'uuid';
+import { requestContextStorage } from '../services/request-context.service.js';
+
+export function requestIdMiddleware(req: Request, res: Response, next: NextFunction): void {
+  const requestId = (req.headers['x-request-id'] as string) || uuidv4();
+  req.requestId = requestId;
+  res.setHeader('x-request-id', requestId);
+
+  requestContextStorage.run({ requestId }, () => {
+    next();
+  });
+}
+export default requestIdMiddleware;
